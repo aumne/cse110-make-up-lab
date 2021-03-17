95b82c1945969612e1dc4a7580ff8ba6c5a4d3c0
aumne <50233978+aumne@users.noreply.github.com>
2021-03-16 20:20:36 -0700

    first "draft" of program

---
+++ main.js
@@ -0,0 +1,9 @@
+const input = document.getElementById('number_input');
+const output = document.getElementById('number_output');
+
+input_form.addEventListener('submit', increment_input);
+function increment_input(event) {
+    event.preventDefault();
+    //TODO error handling
+    output.textContent = input.value++;
+}
